a6f30576b9645fef777ddeb2ce3f48dcc26c424f
ChongZhiJie <ChongZhiJie021604@gmail.com>
2026-05-25 15:25:41 +0800

    Update to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ChongZhiJie0216.github.io',
-	base: '/Notes',
-	integrations: [
-		starlight({
-			title: 'Notes',
-			customCss: [
-				'./src/styles/custom.css',
-			],
-			components: {
-				Footer: './src/components/Footer.astro',
-			},
-			sidebar: [
-				{ autogenerate: { directory: '' } }
-			],
-		}),
-	],
+  site: "https://ChongZhiJie0216.github.io",
+  base: "/Notes/",
+  integrations: [
+    starlight({
+      title: "Notes",
+      customCss: ["./src/styles/custom.css"],
+      components: {
+        Footer: "./src/components/Footer.astro",
+      },
+      sidebar: [{ autogenerate: { directory: "" } }],
+    }),
+  ],
 });
